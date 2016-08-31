ce98591b8d25a3cf6962dcfa9b32ed3ee4393628
Dmitry96 <basher9696@gmail.com>
2016-08-31 16:59:49 +0300

    abra

--- app/overrides/update_header.rb
+++ app/overrides/update_header.rb
@@ -1,3 +1,14 @@
 Deface::Override.new(:virtual_path => "spree/shared/_header", 
                      :name => "change header add Hello World", 
                      :replace => "figure#logo") do "<h1 id='logo'> Hello </h1>" end
+
+
+Deface::Override.new(:virtual_path => "spree/layouts/spree_application", 
+                    :name => "add footer", 
+                    :insert_after => "body") do 
+					'<footer class="footer">
+ 						<div class="container">
+  	 						<p class="text-muted">Place sticky footer content here.</p>
+ 						</div>
+					</footer>'
+  				end
