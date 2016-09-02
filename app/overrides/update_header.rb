111f1edd8bb3ee84d611ae635df4368557820ff6
Dmitry96 <basher9696@gmail.com>
2016-09-02 17:04:38 +0300

    changes

--- app/overrides/update_header.rb
+++ app/overrides/update_header.rb
@@ -5,11 +5,7 @@ Deface::Override.new(:virtual_path => "spree/shared/_header",
 
 
 
-Deface::Override.new(:virtual_path => "spree/shared/_taxonomies", 
-                     :name => "change taxon partioal", 
-                     :replace => "h4.taxonomy-root") do 
-											"<a>Hello</a>"
-end
+
 
 
 # <%= taxons_tree(taxonomy.root, @taxon, max_level) %>
